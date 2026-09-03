b02f3a3b9a915586ab5d68768499eb6eb0865d78
Emma Daujat <emmalouised75@gmail.com>
2026-09-03 15:02:45 +1000

    fix sticker dimension issue

--- src/components/ui/Sticker.tsx
+++ src/components/ui/Sticker.tsx
@@ -7,7 +7,7 @@ type StickyTapeProps = {
 export default function Sticker({
   rotation = 0,
   bgColor = "bg-stickytape-pink",
-  size = "w-20 sm:w-25 h-3 sm:h-6",
+  size = "w-20 sm:w-25 h-6 sm:h-10",
 }: StickyTapeProps) {
   return (
     <div
