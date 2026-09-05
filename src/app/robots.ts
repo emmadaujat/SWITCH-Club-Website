ccdad929980a8f0e9751e2b9b068f84c57eab650
Emma Daujat <emmalouised75@gmail.com>
2026-09-05 17:05:26 +1000

    Add a sitemap and robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://switchrmit.vercel.app/sitemap.xml", // same URL as above
+  };
+}
